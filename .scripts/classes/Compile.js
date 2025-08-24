b0baf2fb09e8503b8c87ae2945d5781c9b0758e9
Maddi Mathon <97260839+maddimathon@users.noreply.github.com>
2025-08-24 16:43:31 -0400

    [2025-08-24] Updated scripts and utility-sass
    Details:
    - Updated with utility-sass html template
    - Converted build scripts back to js

---
+++ .scripts/classes/Compile.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+// @ts-check
+'use strict';
+/*
+ * @package @maddimathon/utility-astro
+ * @author Maddi Mathon (www.maddimathon.com)
+ * 
+ * @license MIT
+ */
+
+/**
+ * @import { Stage } from "@maddimathon/build-utilities"
+ */
+
+import {
+    CompileStage,
+} from '@maddimathon/build-utilities';
+
+/**
+ * Extension of the built-in one.
+ */
+export class Compile extends CompileStage {
+
+    /**
+     * @type {Stage.SubStage.Compile[]}
+     * 
+     * @override
+     * @readonly
+     */
+    subStages = [
+        'ts',
+        'scss',
+        // @ts-expect-error
+        'astro',
+        // @ts-expect-error
+        'templates',
+        'files',
+    ];
+
+
+    /**
+     * @protected
+     */
+    async astro() {
+
+        const subDir = 'astro';
+
+        const distDir = this.getDistDir( undefined, subDir ).trim().replace( /\/$/g, '' );
+
+        await this.runCustomDirCopySubStage( subDir );
+
+        // TODO - remove this when unneeded
+        this.fs.delete( [
+            distDir + '/components/NewComponent.astro',
+            distDir + '/components/NewComponent.d.ts',
+        ], 2 );
+    }
+
+    /**
+     * @protected
+     * @override
+     */
+    async scss() {
+        await this.runCustomDirCopySubStage( 'scss' );
+    }
+
+    /**
+     * @protected
+     */
+    async templates() {
+
+        await this.runCustomScssDirSubStage(
+            'scss/templates',
+            this.getDistDir( undefined, 'css/templates' ),
+            {
+                postCSS: true,
+            },
+        );
+    }
+}
---
+++ .scripts/classes/Document.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+// @ts-check
+'use strict';
+/*
+ * @package @maddimathon/utility-astro
+ * @author Maddi Mathon (www.maddimathon.com)
+ * 
+ * @license MIT
+ */
+
+/**
+ * @import { Stage } from "@maddimathon/build-utilities"
+ */
+
+import {
+    DocumentStage,
+} from '@maddimathon/build-utilities';
+
+/**
+ * Extension of the built-in one.
+ */
+export class Document extends DocumentStage {
+
+    /**
+     * Paths to typedoc outputs.
+     * 
+     * @readonly
+     */
+    static typeDoc_paths = {
+        json: './src/docs/content/typedoc.json',
+        md: './src/docs/content/api',
+    };
+
+
+    /**
+     * @type {Stage.SubStage.Document[]}
+     * 
+     * @readonly
+     * @override
+     */
+    subStages = [
+        // 'typeDoc',
+        // @ts-expect-error
+        'scss',
+        // @ts-expect-error
+        'astro',
+        'replace',
+    ];
+
+
+    /**
+     * @protected
+     * @override
+     */
+    async typeDoc() {
+        this.fs.delete( [
+            Document.typeDoc_paths.json,
+            Document.typeDoc_paths.md,
+        ], 1 );
+
+        await super.typeDoc();
+
+
+        this.console.verbose( 'making replacements in markdown...', 2 );
+        const replacePaths = [
+            Document.typeDoc_paths.json,
+            Document.typeDoc_paths.md + '/**/*',
+        ];
+
+        this.replaceInFiles( replacePaths, 'current', this.params.verbose ? 3 : 2 );
+        this.replaceInFiles( replacePaths, 'package', this.params.verbose ? 3 : 2 );
+
+        this.console.verbose( 'replacing markdown paths...', 2 );
+        this.fs.replaceInFiles( replacePaths, [
+            [ /(?<=\[[^\]]+\]\([^\)]+)\.md\)/gi, '.html)' ],
+        ], this.params.verbose ? 3 : 2 );
+
+
+        this.console.verbose( 'tidying up...', 2 );
+        this.fs.delete( [
+            Document.typeDoc_paths.md + '/.nojekyll',
+            Document.typeDoc_paths.md + '/index.md',
+        ], 1 );
+    }
+
+
+    /**
+     * @protected
+     */
+    async scss() {
+
+        const outDir = (
+            this.getSrcDir( 'docs', 'css' )[ 0 ]
+            ?? this.getSrcDir( undefined, 'docs/css' )
+        ).replace( /\/$/g, '' );
+
+        await this.runCustomScssDirSubStage( 'docs/scss', outDir );
+    }
+
+    /**
+     * @protected
+     */
+    async astro() {
+        this.console.progress( 'building astro docs...', 1 );
+
+        const distDir = this.getDistDir( 'docs' ).trim().replace( /\/$/g, '' );
+
+        if ( this.fs.exists( distDir ) ) {
+            this.console.verbose( 'deleting existing files...', 2 );
+            this.fs.delete( distDir, this.params.verbose ? 3 : 2 );
+        }
+
+        this.console.verbose( 'checking astro types...', 2 );
+        this.try(
+            this.console.nc.cmd,
+            this.params.verbose ? 3 : 2,
+            [ 'astro check' ]
+        );
+
+        this.console.verbose( 'compiling to ' + distDir + '...', 2 );
+        this.try(
+            this.console.nc.cmd,
+            this.params.verbose ? 3 : 2,
+            [ 'astro build' ]
+        );
+    }
+}
