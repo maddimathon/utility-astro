4dd6b18bc84d37355eef1b5079bde91b12552ee0
Maddi Mathon <97260839+maddimathon@users.noreply.github.com>
2025-09-21 16:18:55 -0400

    [2025-09-21] Improved layout scss and ToggleNavMenu
    Details:
    - New ToggleNavMenu component with styles
    - Completely fixed, simple, elegant layouts scss

---
+++ dist/astro/components/ToggleNavMenu.d.ts
@@ -0,0 +1,19 @@
+/**
+ * @since 0.1.0-alpha.draft
+ * 
+ * @packageDocumentation
+ */
+/*!
+ * @maddimathon/utility-astro@0.1.0-alpha.draft
+ * @license MIT
+ */
+
+import type { Props as NavMenuProps } from './NavMenu.d.ts';
+
+/**
+ * Input props for the ToggleNavMenu component.
+ * 
+ * @since 0.1.0-alpha.draft
+ */
+export interface Props extends NavMenuProps {
+}
---
+++ src/astro/components/ToggleNavMenu.d.ts
@@ -0,0 +1,19 @@
+/**
+ * @since ___PKG_VERSION___
+ * 
+ * @packageDocumentation
+ */
+/*!
+ * @maddimathon/utility-astro@___CURRENT_VERSION___
+ * @license MIT
+ */
+
+import type { Props as NavMenuProps } from './NavMenu.d.ts';
+
+/**
+ * Input props for the ToggleNavMenu component.
+ * 
+ * @since ___PKG_VERSION___
+ */
+export interface Props extends NavMenuProps {
+}
