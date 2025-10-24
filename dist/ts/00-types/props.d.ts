ab833cf67b77da27f91a49c7d7b2ac21058e8ecd
Maddi Mathon <97260839+maddimathon@users.noreply.github.com>
2025-10-23 22:07:20 -0400

    [2025-10-23] Toggle displayHeading
    Details:
    - Added displayHeading prop to Toggle (passed to heading)
    - Added ElementProps utility type

--- dist/ts/00-types/props.d.ts
+++ dist/ts/00-types/props.d.ts
@@ -31,6 +31,19 @@ export type GenericProps<T_Props> = T_Props & {
  * export type Props = GenericElementProps<PageProps, "div"|"a">;
  * ```
  */
+export type ElementProps<T_Props, T_HtmlTag extends HTMLTag, T_OmitKeys extends number | string = keyof T_Props & number | string> = T_Props & Partial<Omit<HTMLAttributes<T_HtmlTag>, T_OmitKeys | "class" | "class:list">> & {
+    'aria-description'?: string;
+    class?: ClassList;
+};
+/**
+ * Creates a props object for components, with attributes inherited from the
+ * HTML attributes for the given tag(s).
+ *
+ * @example
+ * ```ts
+ * export type Props = GenericElementProps<PageProps, "div"|"a">;
+ * ```
+ */
 export type GenericElementProps<T_Props, T_HtmlTag extends HTMLTag> = GenericProps<T_Props & {
     class?: ClassList;
 }> & Partial<Omit<HTMLAttributes<T_HtmlTag>, keyof T_Props | "class" | "class:list">> & {
--- src/ts/00-types/props.ts
+++ src/ts/00-types/props.ts
@@ -33,6 +33,27 @@ export type GenericProps<T_Props> = T_Props & {
  * export type Props = GenericElementProps<PageProps, "div"|"a">;
  * ```
  */
+export type ElementProps<
+    T_Props,
+    T_HtmlTag extends HTMLTag,
+    T_OmitKeys extends number | string = keyof T_Props & number | string,
+> = T_Props & Partial<Omit<
+    HTMLAttributes<T_HtmlTag>,
+    T_OmitKeys | "class" | "class:list"
+>> & {
+    'aria-description'?: string;
+    class?: ClassList;
+};
+
+/**
+ * Creates a props object for components, with attributes inherited from the
+ * HTML attributes for the given tag(s).
+ *
+ * @example
+ * ```ts
+ * export type Props = GenericElementProps<PageProps, "div"|"a">;
+ * ```
+ */
 export type GenericElementProps<
     T_Props,
     T_HtmlTag extends HTMLTag,
